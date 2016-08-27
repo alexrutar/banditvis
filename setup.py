5ca417ee638720aedeed587a137b23cd1e17972c
Alex Rutar <alexrutarhome@gmail.com>
2016-08-27 17:56:46 -0600

    uploaded version 0.3 to pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
 
 setup(
     name='banditvis',
-    version='0.2',
+    version='0.3',
     description='A library for simulating and visualizing bandit algorithms.',
     long_description=readme(),
     url='https://github.com/alexrutar/banditvis',
